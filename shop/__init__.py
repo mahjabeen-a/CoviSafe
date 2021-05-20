58ca6379998f6bed1d555afaffec7e0526565248
mahjabeen-a <mahjabeen.aka@gmail.com>
2021-05-20 23:41:36 +0530

    added db

--- shop/__init__.py
+++ shop/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt 
 
 app = Flask(__name__)
 #URI - Uniform Resource Identifier
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///covisafe.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'spooprathmahj142815'
 db = SQLAlchemy(app)
-
+bcrypt = Bcrypt(app)
 from shop.admin import routes
