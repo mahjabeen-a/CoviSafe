35c95fa584f714a9b9b45190eaf337a545db5d7b
mahjabeen-a <mahjabeen.aka@gmail.com>
2021-05-21 12:49:52 +0530

    added login

--- shop/admin/routes.py
+++ shop/admin/routes.py
@@ -1,10 +1,10 @@
 from flask import render_template,session, request, redirect, url_for, flash
 from shop import app, db, bcrypt
-from .forms import RegistrationForm
+from .forms import RegistrationForm, LoginForm
 from .models import User
 
-@app.route('/')
-def home():
+@app.route('/admin')
+def admin():
     return render_template('admin/index.html', title='Admin Page')
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -19,3 +19,17 @@ def register():
         flash(f'Welcome {form.name.data} ! Thanks for registering', 'success')
         return redirect(url_for('home'))
     return render_template('admin/register.html', form=form, title="Registration Page")
+
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    form = LoginForm(request.form)
+    if request.method == 'POST' and form.validate():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            session['email'] = form.email.data
+            flash(f'Welcome {form.email.data} ! You are loggedin now','success')
+            return redirect(request.args.get('next') or url_for('admin'))
+        else:
+            flash('Wrong email or password', 'danger')
+    return render_template('admin/login.html',title='Login page',form=form)
