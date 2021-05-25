35aec4f69f2714c7d4655fa9effd96df9b503e16
Prathiba28 <dgprathiba28@gmail.com>
2021-05-25 16:57:40 +0530

    Customer logout

--- shop/customers/routes.py
+++ shop/customers/routes.py
@@ -34,3 +34,9 @@ def customerLogin():
         flash('Incorrect email and password','danger')
         return redirect (url_for('customerLogin'))
     return render_template('customer/login.html' ,form=form)
+
+@app.route('/customer/logout')
+def customer_logout():
+    logout_user()
+    return redirect(url_for('home'))
+
