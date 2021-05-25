02212594435bc20e41fbc88f3b67c4751ee4cb95
mahjabeen-a <mahjabeen.aka@gmail.com>
2021-05-25 09:30:08 +0530

    customer registration, and saving to database

--- shop/customers/routes.py
+++ shop/customers/routes.py
@@ -2,11 +2,19 @@
 
 from shop.admin.routes import category
 from flask import request, session, url_for, redirect, render_template, flash, current_app
-from shop import app, db, photos, products,search
+from shop import app, db, photos, search, bcrypt
 from .forms import CustomerRegistrationForm
+from .models import Register
 import secrets, os
 
 @app.route('/customer/register', methods=['GET', 'POST'])
 def customer_register():
     form = CustomerRegistrationForm(request.form)
+    if request.method == 'POST':
+        hash_password = bcrypt.generate_password_hash(form.password.data)
+        register = Register(name=form.name.data, username=form.username.data, email=form.email.data,password=hash_password,state=form.state.data, city=form.city.data,contact=form.contact.data, address=form.address.data, pincode=form.pincode.data)
+        db.session.add(register)
+        flash(f'Welcome {form.name.data} Thank you for registering!', 'success')
+        db.session.commit()
+        return redirect(url_for('login'))
     return render_template('customer/register.html', form=form)
